ecf6619ba8459fe18d3d2bc8276fdd07d2c9ac72
pawgli <pawel.gliniecki@droidsonroids.pl>
2024-04-12 15:19:15 +0200

    add all the plugins to classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 plugins {
-  alias(libs.plugins.android.app) apply false
-  alias(libs.plugins.android.library) apply false
-  alias(libs.plugins.kotlin.android) apply false
-  alias(libs.plugins.kotlin.multiplatform) apply false
+  with(libs.plugins) {
+    alias(android.app) apply false
+    alias(android.library) apply false
+    alias(kotlin.android) apply false
+    alias(kotlin.multiplatform) apply false
+    alias(kotlin.serialization) apply false
+    alias(sqlDelight) apply false
+
+    // Convention plugins
+    alias(convention.android.app) apply false
+    alias(convention.android.app.compose) apply false
+    alias(convention.android.library) apply false
+    alias(convention.android.library.compose) apply false
+    alias(convention.multiplatform) apply false
+    alias(convention.multiplatform.koin) apply false
+    alias(convention.multiplatform.ktor) apply false
+    alias(convention.multiplatform.sqlDelight) apply false
+  }
 }
